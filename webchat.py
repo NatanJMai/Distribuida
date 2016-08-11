47cf5b8ca74de109ef6c33c77f090c80f44168a9
NatanJMai <natan.mai@hotmail.com>
2016-08-11 15:08:01 -0300

    Insert second work

--- webchat.py
+++ webchat.py
@@ -1,24 +1,63 @@
-from bottle import route, run, template, get, request, post, redirect, view
+import bottle
+import json
+import threading
+import requests
+import time
 from sys import argv
 
-list_messages = []
+l_messages = [argv[-1]]
+l_clients  = argv[2:-1]
 
-@get('/')
-@view('index')
+@bottle.get('/')
+@bottle.view('index')
 def message():
-     return {'list_messages': list_messages}
+     return {'list_messages': l_messages}
 
-@post('/message')
+@bottle.post('/message')
 def receive_message():
-    source = request.forms.get("Remetente")
-    target = request.forms.get("Destinatario")
-    subjec = request.forms.get("Assunto")
-    messag = request.forms.get("Mensagem")
+    source = bottle.request.forms.get("Remetente")
+    target = bottle.request.forms.get("Destinatario")
+    subjec = bottle.request.forms.get("Assunto")
+    messag = bottle.request.forms.get("Mensagem")
 
     if source != "" and target != "":
-        list_messages.append((source, target, subjec, messag))
+        l_messages.append((source, target, subjec, messag))
     redirect('/')
 
-print(argv[1])
-print(int(argv[1]))
-run(host="localhost", port=int(argv[1]))
+@bottle.route('/list_peers')
+def search_peers():
+    return json.dumps(l_clients)
+
+@bottle.route('/list_messages')
+def search_messages():
+    return json.dumps(l_messages)
+
+
+def t_run():
+    time.sleep(4)
+    lst_p = []
+    lst_m = []
+
+    while True:
+        for p in l_clients:
+            peers = requests.get(p + '/list_peers')
+            messg = requests.get(p + '/list_messages')
+
+            lst_p = l_clients  + json.loads(peers.text)
+            lst_m = l_messages + json.loads(messg.text)
+
+            time.sleep(1)
+            
+        l_clients[:]  = list(set(lst_p + l_clients))
+        l_messages[:] = list(set(lst_m + l_messages))
+        print(l_clients, l_messages)
+
+
+def main():
+    t = threading.Thread(target=t_run)
+    t.start()
+    #print(list_clients)
+    bottle.run(host="localhost", port=int(argv[1]))
+
+if __name__ == '__main__':
+    main()
